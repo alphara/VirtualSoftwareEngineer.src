8c0a49d146a60ce1d03c632bacace0a34d5dbecf
alpharak <artarakcheev@gmail.com>
2018-03-29 19:54:25 -0300

    Write initial text

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <h1>
+            Self-Developing AI
+          </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p>
+I'm architecting with passion the Self-Developing AI. This is a Virtual Software Engineer, which develops any kind of software include backend and frontend, deep learning and neural networks, artificial intelligence and itself. The Artificial Superintelligence is able to improve its own code.
+        </p>
+        <p>
+The Self-Developing AI is secure and controlled by mankind. It recieves input with application specification on human language and outputs well developed and tested working code of the application on programming language. AI deeply learns programming by itself through reading documentation in the Internet. The project assumes psycological modeling of the best developers and software development. The Virtual Software Engineer could implement dreams of all people in the World. 🦄 
+        </p>
+        <p>
+I'm looking for Co-Founders, Investors, and team members.
+        </p>
+        <p>
+Please,
+        </p>
+        <p>
+Sincerely,
+Artem Arakcheev.
         </p>
       </div>
     );
